d5a71fd5c6e2904c3bef371d36b57b2cac638b21
JosueRFP <josue.petri.senac@gmail.com>
2025-07-08 11:32:00 -0300

    Importação do TMPro

--- Gacha System/Assets/Scripts/GachaController.cs
+++ Gacha System/Assets/Scripts/GachaController.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
 
+public enum Rarity 
+{ 
+    None, Star3, Star4, Star5, Ultra
+}
+
+
 public class GachaController : MonoBehaviour
 {
+    [SerializeField] GameObject[] characters;
+    [SerializeField] float ghostPersentage;
+
+    bool getNewCharacter = false;
+    Rarity rarity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        characters = new GameObject[characters.Length];
+
+        for (int i = 0; i < characters.Length; i++) 
+        {
+            ghostPersentage += 10;
+        }
     }
 
     // Update is called once per frame
@@ -13,4 +29,6 @@ public class GachaController : MonoBehaviour
     {
         
     }
+
+    
 }
