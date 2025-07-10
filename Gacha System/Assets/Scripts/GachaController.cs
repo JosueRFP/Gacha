bc64a820037b4373e7747655c9a48a8fe45490fa
JosueRFP <josue.petri.senac@gmail.com>
2025-07-10 16:38:26 -0300

    Retirada de Assets

--- Gacha System/Assets/Scripts/GachaController.cs
+++ Gacha System/Assets/Scripts/GachaController.cs
@@ -1,34 +1,9 @@
 using UnityEngine;
 
-public enum Rarity 
-{ 
-    None, Star3, Star4, Star5, Ultra
-}
 
 
 public class GachaController : MonoBehaviour
 {
-    [SerializeField] GameObject[] characters;
-    [SerializeField] float ghostPersentage;
-
-    bool getNewCharacter = false;
-    Rarity rarity;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        characters = new GameObject[characters.Length];
-
-        for (int i = 0; i < characters.Length; i++) 
-        {
-            ghostPersentage += 10;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     
 }
+// https://www.youtube.com/watch?v=Lyc-ixiSSRY como refêrencia
